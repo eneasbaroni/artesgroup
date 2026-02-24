d3db885f44dcb44f8698f674bf7424d142e49393
Eneas Baroni <eneasbaroni@gmail.com>
2026-02-24 16:04:19 -0300

    feat: about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,15 +7,6 @@ import {
 } from "./constants";
 import Events from "../views/Events/Events";
 
-/* const fadeInAnimation = {
-    initial: { opacity: 0, y: 100 },
-    animate: (index: number) => ({
-        opacity: 1,
-        y: 0,
-        transition: { delay: index * 0.05 },
-    }),
-}; */
-
 const variants = {
     initial: { opacity: 0, y: 100 },
     animation: {
