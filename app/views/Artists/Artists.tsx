d58a3da9e02ad15476187d260af18fbbc328b9a5
Eneas <87401725+eneasbaroni@users.noreply.github.com>
2026-02-26 11:58:20 -0300

    fix swipe position

--- app/views/Artists/Artists.tsx
+++ app/views/Artists/Artists.tsx
@@ -4,17 +4,17 @@ import { ARTISTS } from "./constants";
 
 const Artists = () => {
   return (
-    <div className="w-full h-screen flex flex-col justify-center">
+    <div className="w-full h-screen flex flex-col justify-center relative">
       <h2 className="ml-15 tablet:ml-10 font-anton text-[40px] mobile:text-3xl">
         NUESTROS ARTISTAS
       </h2>
-      <div className="w-full h-94 mobile:h-70 flex overflow-x-auto scrollbar-custom relative">
+      <div className="w-full h-94 mobile:h-70 flex overflow-x-auto scrollbar-custom">
         {ARTISTS.map((artist) => (
           <ArtistInfo key={artist.name} {...artist} />
         ))}
-        <div className="w-auto h-auto inline-block md:hidden absolute bottom-4 left-[calc(100vw-40px)]">
-          <Swipe />
-        </div>
+      </div>
+      <div className="w-auto h-auto inline-block md:hidden absolute top-100 left-[calc(100vw-40px)]">
+        <Swipe />
       </div>
     </div>
   );
